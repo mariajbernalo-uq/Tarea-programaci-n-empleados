5f823f2096487eb7d92c1f81b091fa927bd4c537
mariajbernalo-uq <mariaj.bernalo@uqvirtual.edu.co>
2026-04-14 13:22:43 -0500

    Update ResumenPago.java

--- Empresaempleados/src/ResumenPago.java
+++ Empresaempleados/src/ResumenPago.java
@@ -1,3 +1,3 @@
-public record ResumenPago(String nombre, String documento, String tipoEmpleado, float salarioBruto, float descuento, float salarioNeto) {
+public record ResumenPago(String nombre, String documento, String tipoEmpleado, float salarioBruto, float descuentos, float salarioNeto) {
 
 }
